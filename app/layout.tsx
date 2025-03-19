4202319f6585aca9f1b104d2126478bdcf2c861c
mr-engineer49 <eneahysa49@gmail.com>
2025-03-19 10:41:41 +0100

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="gb">
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
